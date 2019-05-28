e62f9e1449539d224be8717c810e1b6d4407f162
hpoleselo <hpoleselo@gmail.com>
2019-05-28 20:46:51 -0300

    Logging to raspberry pi

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ Copy the file to the folder you want to convert.
 ## .png to .jpg (convert.py)
 
 Drag this file to the wished folder and just run it, it will do the conversion automatically.
+
+## Automated login to Raspberry Pi (pipass.py)
+
+Logins in to the RPi with aid of SSH, the script just opens the file which contains the password in form of text. It was pretty annoying at the time because i always forgot the IP and too tired for typing password in.
---
+++ pipass.py
@@ -0,0 +1,16 @@
+from subprocess import call
+import re
+
+with (open("pipassword.txt")) as filee:
+    lines = filee.readlines()
+    # Remove the \n in the txt file
+    lines_wout_n = [s.replace('\n','') for s in lines]
+    print lines_wout_n
+    username = lines_wout_n[0]
+    ip_address = lines_wout_n[1]
+    pwd = lines_wout_n[2]
+    command = "ssh" + " " + username + "@" + ip_address
+    # In order to use the call function we have to pass the commands as strings from a list! Split makes that work for us.
+    command = command.split()
+    print command
+    call(command, shell=False)
