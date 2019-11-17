2a5c399ecf935331dca56395920f6298ec9cb567
hpoleselo <hpoleselo@gmail.com>
2019-11-16 20:18:56 -0500

    Fixed error when plotting the y-axis, now everything is working properly

--- ArduinoSerialPy/plotLive.py
+++ ArduinoSerialPy/plotLive.py
@@ -12,9 +12,10 @@ fig = plt.figure()
 ax1 = fig.add_subplot(1,1,1)
 
 def animate(i):
-    # i eh o intervalo
+    # i eh o intervalo, por alguma razao o i eh passado toda vez que um ponto novo vem
     graph_data = open('example.txt' , 'r').read()
     lines = graph_data.split('\n')
+    print(lines)
 
     xs = []
     ys = []
@@ -24,12 +25,13 @@ def animate(i):
         if len(line)>1:
             # , pois o arquivo eh separado por virgula
             x, y = line.split(',')
-            xs.append(x)
-            ys.append(y)
+            # Os dados precisam ser plotados como floats de fato e nao como strings
+            xs.append(float(x))
+            ys.append(float(y))
     # Limpa tudo antes de plotar
     ax1.clear()
     ax1.plot(xs,ys)
 
 # qual figura queremos animar, qual funcao eo intervalo em ms que o grafico vai dar update
-animt = animation.FuncAnimation(fig, animate, interval=5000)
+animt = animation.FuncAnimation(fig, animate, interval=500)
 plt.show()
