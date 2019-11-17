278e307962974f2face99a770adcc01bfdeb5e6a
hpoleselo <hpoleselo@gmail.com>
2019-11-16 20:19:27 -0500

    Adapted new way to plot data and addded new methods. Test how the data is incoming and adapt the algorightm

--- ArduinoSerialPy/dataReceiver.py
+++ ArduinoSerialPy/dataReceiver.py
@@ -1,5 +1,7 @@
 import serial
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
 import numpy as np
 
 
@@ -9,27 +11,20 @@ class MeasurementPlotter(object):
         # Arduino is set always in linux to /dev/ttyUSB0, baudrate matches Arduino's
         self.ser = serial.Serial('/dev/ttyUSB0', 9600)
         print("Using port: %s") %self.ser.name
-        self.checkConnectivity()
 
-    def checkConnectivity(self):
+        # Setup for plotting in Matplotlib
+        style.use('fivethirtyeight')
+        self.xs = []
+        self.ys = []
+
+
+    def readFromSerial(self):
         try:
             while True:
                 self.connected = True
-                plt.ion()                    #sets plot to animation mode
-
-                # How many points we want to retrieve from the readings
-                self.length = 500
-
-                # Creating the variable before we store the values to be read
-                values = [0]*self.length
-
-                #xline = plt.plot(t)
-                yline, = plt.plot(values)
-                plt.ylim(400,700)   # Sets the y axis limits
 
                 # This will parse through length and read the serial values
                 for i in range(len(values)):
-                    print("Entrou no for")
                     # Reads until it gets a carriage return. MAKE SURE THERE IS A CARRIAGE RETURN OR IT READS FOREVER
                     data = self.ser.readline()
                     # Splits string into a list at the tabs   
@@ -37,20 +32,28 @@ class MeasurementPlotter(object):
                     print(separate)
                 
                     values.append(int(separate[0]))   #add new value as int to current list
-                
-                    del values[0]
-                
-                    #xline.set_xdata(np.arange(len(x))) #sets xdata to new list length
-                    yline.set_ydata(np.arange(len(values)))
-                
-                    plt.pause(1000)                   #in seconds
-                    plt.draw()                         #draws new plot
-                        
         except:
             self.ser.close()
 
 
-    # ADD THIS LATER
+    def plotData(self,i):
+        #TODO: Ver como ler a informacao da serial
+        # Para esse caso o dado vem dessa forma: ['0,2', '1,4', '2,6', '3,8', '4,10', '5,12', '6,14', '7,16', '8,18', '9,20', '10,3', '11,-5', '12,-10']
+        graph_data = open('example.txt' , 'r').read()
+        lines = graph_data.split('\n')
+
+        for line in lines:
+            # Trim the empty lines
+            if len(line)>1:
+                # Assuming we're reading a file that contains commas
+                x, y = line.split(',')
+                # The data must me plotted as float not as a string!
+                self.xs.append(float(x))
+                self.ys.append(float(y))
+        # Clean everything before we plot
+        ax1.clear()
+        ax1.plot(xs,ys)
+
     def saveData(self):
         # Combines lists together
         rows = zip(values, t)
@@ -60,5 +63,12 @@ class MeasurementPlotter(object):
 
         # Saves data in file (load w/np.loadtxt())
         np.savetxt("C:\\Users\\mel\\Documents\\Instructables\\test_radio2.txt", row_arr)
+
+    def main(self):
+        self.readFromSerial()
+        #animt = animation.FuncAnimation(fig, self.plotData, interval=500)
+        #plt.show()
+
+
 
 MeasurementPlotter()
