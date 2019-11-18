d800c0c02141eb67698b067612ad703ca4c2d567
Henrique Poleselo <hpoleselo@gmail.com>
2019-11-18 17:15:24 -0300

    Sending the time AND the sensor values as CSV

--- ArduinoSerialPy/dataTransmittere/dataTransmittere.ino
+++ ArduinoSerialPy/dataTransmittere/dataTransmittere.ino
@@ -1,13 +1,17 @@
 // Analog Pin A0 from the potentiometer (the middle pin from the potentiometer)
 const int potentiometer = 0;
 int value = 0;
+int actualTime = 0;
  
 void setup() {
   Serial.begin(9600);
 }
  
 void loop() {
+  actualTime = millis();
   value = analogRead(potentiometer);
+  Serial.print(actualTime);
+  Serial.print(",");
   Serial.println(value);
-  delay(10);
+  delay(100);
 }
