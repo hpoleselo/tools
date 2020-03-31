f833718a39b84a7feb328f29d4af4ff545f59b3a
Henrique Poleselo <hpoleselo@gmail.com>
2020-03-31 00:39:03 -0300

    Added vlc

--- init.sh
+++ init.sh
@@ -1,1 +1,1 @@
-sudo apt-get install terminator git kazam deluge curl gparted python3-pip python3-dev okular
+sudo apt-get install terminator git kazam deluge curl gparted python3-pip python3-dev okular vlc
