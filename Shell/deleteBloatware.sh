5efb2a2b6eea9cd253fb600b632988a5be16d32e
Henrique Poleselo <hpoleselo@gmail.com>
2020-04-24 13:35:16 -0300

    Added folder to organize shell scripts

---
+++ Shell/deleteBloatware.sh
@@ -0,0 +1,1 @@
+sudo apt-get purge aisleriot thunderbird* transmission-* account-plugin-facebook account-plugin-flickr account-plugin-twitter gnome-mines gnome-mahjongg gnome-sudoku gnome-calendar gnome-www-browser
