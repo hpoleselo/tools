6cec676bfb7acb7570d2c5f37b992ca5a2d893e7
Henrique Poleselo <hpoleselo@gmail.com>
2020-04-26 20:12:24 -0300

    docker added

--- Shell/init.sh
+++ Shell/init.sh
@@ -1,5 +1,5 @@
 # General utilities
-sudo apt-get install terminator git kazam deluge curl gparted python3-pip python3-dev okular vlc filezilla vokoscreen tree gimp \
+sudo apt-get install terminator git kazam deluge curl gparted python3-pip python3-dev okular vlc filezilla vokoscreen tree gimp docker.io \
 
 # Utilites for OpenCV in Ubuntu 16.04
 sudo apt-get install libjpeg8-dev libtiff5-dev libjasper-dev libpng12-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libgtk-3-dev libatlas-base-dev gfortran python2.7-dev python3.5-dev
