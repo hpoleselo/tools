1cf66c8c6f99b8340ae3b5c90e8e706678336249
Henrique Poleselo <hpoleselo@gmail.com>
2020-05-17 16:45:37 -0300

    ffmpeg and todoist link

--- Shell/init.sh
+++ Shell/init.sh
@@ -1,8 +1,8 @@
 # General utilities
-sudo apt-get install terminator git deluge curl gparted python3-pip python3-dev okular vlc filezilla vokoscreen tree gimp docker.io htop mediainfo handbrake \
+sudo apt-get install terminator git deluge curl gparted python3-pip python3-dev okular vlc filezilla vokoscreen tree gimp docker.io htop mediainfo ffmpeg \
 
 # Echoing programs that aren't available in aptitude and i should not forget to install
-echo "\nDon't forget to install: Postman, Telegram, URSim \n"
+echo "\nDon't forget to install: Postman, Telegram, URSim, Todoist https://github.com/KryDos/todoist-linux/releases \n"
 
 # Utilites for OpenCV in Ubuntu 16.04
 #sudo apt-get install libjpeg8-dev libtiff5-dev libjasper-dev libpng12-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libgtk-3-dev libatlas-base-dev gfortran python2.7-dev python3.5-dev
