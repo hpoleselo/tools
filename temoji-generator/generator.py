c47b556037d52264c2bba27903f252f89ce83a4b
Henrique Poleselo <hpoleselo@gmail.com>
2020-07-23 13:39:43 -0300

    Stuck on problem: the method to find the values are based on uint8 but converting the df to str to place the emojis makes the search quite hard (using str.contains)

--- temoji-generator/generator.py
+++ temoji-generator/generator.py
@@ -14,27 +14,37 @@ def generate_letters():
 
     # Convert to pandas dataframe (table)
     df = pd.DataFrame(data=bw_image)
+    df2 = pd.DataFrame(data=bw_image)
 
 
+    x, y = bw_image.shape
+    img_size = x*y
+    print("Tamanho da imagem (numero de pixels):", img_size)
+    print("Numero de zeros: ", len(df.values[(df == 0).values]))
+    print("Numero de 255: ", len(df.values[(df > 0).values]))
 
+    # Since it comes from opencv function, the data type is uint8, we have to implement it to be ...?
+    #print(df.dtypes)
+    # Mudando para ser string, assim conseguimos assinalar como os emojis
+    #df = df.astype(str)
 
-
-    print(len(df.values[(df == 0).values]))
-    df.values[df == 0] = 0.5
-    #df.values[(df == 0).values] = 0.5
-    print(df.values[(df == 0.5).values])
-    print(len(df.values[(df == 0.5).values]))
+    print("-- Aplicando mudança --")
     
     # All values equal to 0 are going to be full_moon
-    df.values[(df == 0).values] = emoji.emojize(':full_moon:')
+    #df.values[(df == 0).values] = emoji.emojize(':full_moon:')
 
     # All values higher then 0 are going to be new_moon
-    df.values[(df > 0).values] = emoji.emojize(':new_moon:')
+    #df.values[(df > 0).values] = emoji.emojize(':new_moon:')
 
-    print(df)
+    #df.values[(df > 0).values] = emoji.emojize(':new_moon:')
 
-    #cv2.imshow("Normal", img)
-    #cv2.imshow("B&W Image",bw_image)
-    #cv2.waitKey(0)
+    # For some reason the following attempt did not work to find all the strings on the data frame:
+    # df.values.str.contains('255') or df.values[df.values.str.contains('255')]
+
+    # Com a função lambda nao funciona pois nao conseguimos fazer assignment
+    #for i in df.columns:
+    #    df[i].astype('str').apply(lambda x: df[ if x.startswith('255') else 'pass')
+
+    # usar tabela auxiliar? df2..
 
 generate_letters()
