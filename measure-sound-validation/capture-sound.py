782a356e3377d92cb7e826d4d0c0bf818adf8bb1
Henrique Poleselo <hpoleselo@gmail.com>
2020-11-24 16:52:39 -0300

    FFT example working

--- measure-sound-validation/capture-sound.py
+++ measure-sound-validation/capture-sound.py
@@ -2,6 +2,7 @@ import sounddevice as sd
 from scipy.io.wavfile import write
 import numpy as np
 from matplotlib import pyplot as plt
+from scipy.fft import rfft, rfftfreq
 
 #sudo apt-get install libportaudio2 in order to use sounddevice
 #sudo apt-get install python3-tk
@@ -18,21 +19,40 @@ def capture_audio():
 
 
 def analyze_audio(audio):
-    pass
+    # Number of samples in normalized_tone
+    N = FS * DURATION
+
+    yf = rfft(audio)
+    xf = rfftfreq(N, 1 / FS)
+
+    plt.plot(xf, np.abs(yf))
+    plt.show()
 
 
 def generate_sine_wave(freq, sample_rate, duration):
     x = np.linspace(0, duration, FS * DURATION, endpoint=False)
     frequencies = x * freq
     # 2pi because np.sin takes radians
+
+    
     y = np.sin((2 * np.pi) * frequencies)
     return x, y
-
 
 def main():
     # Generate a 2 hertz sine wave that lasts for 5 seconds
     x, y = generate_sine_wave(2, FS, DURATION)
-    plt.plot(x, y)
+
+    _, nice_tone = generate_sine_wave(400, FS, DURATION)
+    _, noise_tone = generate_sine_wave(4000, FS, DURATION)
+    noise_tone = noise_tone * 0.3
+
+    mixed_tone = nice_tone + noise_tone
+    normalized_tone = np.int16((mixed_tone / mixed_tone.max()) * 32767)
+
+    plt.plot(normalized_tone[:1000])
     plt.show()
+    analyze_audio(normalized_tone)
+    write("mysinewave.wav", FS, normalized_tone)
+
 
 main()
